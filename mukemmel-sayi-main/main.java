50be276bcb17bf4e0a9c073d2afbf66909940d22
Esra Emine Evren <88592010+esraevren@users.noreply.github.com>
2022-07-08 11:30:03 +0300

    Add files via upload

---
+++ mukemmel-sayi-main/main.java
@@ -0,0 +1,25 @@
+package ornek_31;
+
+import java.util.Scanner;
+
+public class Driver {
+ public static void main(String[] args) {
+	
+	 Scanner scanner=new Scanner(System.in);
+	 System.out.println("Bir sayı giriniz:");
+	 int number,total=0;
+	 number=scanner.nextInt();
+	
+    for(int i=1;i<number;i++) {
+    	if(number%i==0) {
+    		total+=i;
+    	}
+    }
+    if(total==number) {
+    	System.out.println(number+" Mükemmel sayıdır.");
+    }else {
+    	System.out.println(number+" Mükemmel sayı değildir.");
+    }
+   
+}
+}
